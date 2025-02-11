5258fc95081c2f34e6510825c6b06876ebcbc83a
Pedro A. <pedroaugusto732@gmail.com>
2025-02-10 21:37:09 -0300

    Update BusinessException.java

--- src/model/exception/BusinessException.java
+++ src/model/exception/BusinessException.java
@@ -1,6 +1,8 @@
 package model.exception;
 
-public class DomainException extends RuntimeException{
+public class BusinessException extends RuntimeException{
 
-
+    public BusinessException( String msg ) {
+        super(msg);
+    }
 }
