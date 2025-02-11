cd20721a438974ffa3d19bf14e2bd4220cb9a5c5
Pedro A. <pedroaugusto732@gmail.com>
2025-02-10 21:36:16 -0300

    Update program.java

--- src/application/program.java
+++ src/application/program.java
@@ -1,4 +1,43 @@
 package application;
 
+import model.entities.Account;
+import model.exception.BusinessException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class program {
+
+    public static void main( String[] args ) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dados da conta: ");
+        System.out.print("Número: ");
+        int number = sc.nextInt();
+        System.out.print("Titular: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+        System.out.print("Saldo inicial: ");
+        double balance = sc.nextDouble();
+        System.out.print("Limite de saque: ");
+        double withdrawLimit = sc.nextInt();
+
+        Account acc = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.println();
+        System.out.print("Informe uma quantia para sacar: ");
+        double amount = sc.nextDouble();
+
+        try {
+            acc.withdraw(amount);
+            System.out.printf("Novo saldo: %.2f", acc.getBalance());
+        }
+        catch (BusinessException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
 }
